88ee122b973ce7f2d3018e19d6a691b2218a82d6
sarvjot <sarvjotsingh422002@gmail.com>
2021-10-12 15:25:42 +0530

    make script now creates a dir of io

--- scripts/other/make.sh
+++ scripts/other/make.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-for file in "in" "out" "ans"
+[ -d io ] || mkdir io
+for file in "in1" "out1" "ans1" "in2" "out2" "ans2" "in0" "out0" "ans0"
 do
-    [ -f $file ] && rm $file
-    touch $file
+    [ -f io/$file ] && rm io/$file
+    touch io/$file
 done
 
 path="/home/sarvjot/programming/cp/code_lib/templates/cp_templates/template.cpp"
-gf_path="/home/sarvjot/programming/cp/code_lib/templates/cp_templates/template_google.cpp"
 
-[ $1 = "gf" ] && path=$gf_path
-
-for var in ${@:2}
+for var in "$@"
 do
     file="$var.cpp"
     [ -f $file ] && rm $file
