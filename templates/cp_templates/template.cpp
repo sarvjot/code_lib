4f29b48c55c212a917c545d231db123d79d8b87e
sarvjot <sarvjotsingh422002@gmail.com>
2021-10-30 16:28:48 +0530

    add variadic debugging template

--- templates/cp_templates/template.cpp
+++ templates/cp_templates/template.cpp
@@ -1,21 +1,39 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T,typename S,typename Q>ostream &operator<<(ostream &os,tuple<T,S,Q> &t){os<<"("<<get<0>(t)<<", "<<get<1>(t)<<", "<<get<2>(t)<<")";return os;}
+template<typename T,typename S>ostream &operator<<(ostream &os,pair<T,S>&p){os<<"("<<p.first<<", "<<p.second<<")";return os;}
+template<typename T,typename S>ostream &operator<<(ostream &os,map<T,S>&v){os<<"[ ";for(auto it=v.begin();it!=v.end();it++){os<<"("<<(*it).first<<", "<<(*it).second <<") ";}os <<"]";return os;}
+template<typename T>ostream &operator<<(ostream &os,vector<T>&v){os<<"[ ";for(auto element:v){os<<element<<" ";}os<<"]";return os;}
+template<typename T>ostream &operator<<(ostream &os,set<T> &v){os<<"[ ";for(auto it=v.begin();it!=v.end();it++){os<<*it<<" ";}os<<"]";return os;}
+template<typename T>ostream &operator<<(ostream &os,multiset<T> &v){os<<"[ ";for(auto it=v.begin();it!=v.end();it++){os<<*it<<" ";}os<<"]";return os;}
+template<typename...Args>void logger(string vars,Args&&...values){cerr<<"\033[31m"<<vars<<" : ";string delim;(...,(cerr<<delim<<values,delim=", "));cerr<<"\033[37m"<<endl;}
 
 #define nl "\n"
 #define big long long
 #define precision(x) fixed << setprecision(x)
 #define get_set_bits(x) __builtin_popcountll(x) 
+#define ordered tree<big, null_type, less<big>, rb_tree_tag, tree_order_statistics_node_update>
+#define ordered_ms tree<pair<big, big>, null_type, less<pair<big, big>>, rb_tree_tag, tree_order_statistics_node_update>
 
-#ifndef ONLINE_JUDGE
+#ifdef SARVJOT 
 #define ds(statement) cerr << "\033[32m" << statement << "\033[37m" << endl
-#define dv(var) cerr << "\033[34m" << (#var) << ": " << var << "\033[37m" << endl
+#define dv(args...) logger(#args, args)
 #define sm_line  cerr << "\033[33m" << "---------X--------" << "\033[37m" << endl
-#define lg_line  cerr << "\033[31m" << "------X----------X------" << "\033[37m" << endl
+#define lg_line  cerr << "\033[34m" << "------X----------X------" << "\033[37m" << endl
+void print_pbds(ordered &v){cerr<<"\033[31m"<<"[ ";for(auto a:v){cerr<<a<<" ";}cerr<<"]"<<"\033[37m"<<endl;}
+void print_pbdsms(ordered_ms &v){cerr<<"\033[31m"<<"[ ";for(auto a:v){cerr<<a<<" ";}cerr<<"]"<<"\033[37m"<<endl;}
 #else
 #define ds(statement)
-#define dv(var)
+#define dv(...)
 #define sm_line
 #define lg_line
+void print_pbds(ordered &v){;}
+void print_pbdsms(ordered_ms &v){;}
 #endif
 
 const big MOD = 1e9 + 7;
